e4d477b53e077df4895168697cf86ca8c75c5177
Luca Cantoreggi <luca@cn7r66.com>
2023-04-11 18:01:23 +0200

    Add IsType assertion

--- src/Assertion/String/IsType.php
+++ src/Assertion/String/IsType.php
@@ -1,13 +1,12 @@
 <?php
 
-declare(strict_types=1);
-
 /*
  * This file is part of Vivarium
  * SPDX-License-Identifier: MIT
  * Copyright (c) 2023 Luca Cantoreggi
- *
  */
+
+declare(strict_types=1);
 
 namespace Vivarium\Assertion\String;
 
@@ -26,7 +25,7 @@ final class IsType implements Assertion
         if (! $this($value)) {
             $message = sprintf(
                 ! (new IsEmpty())($message) ?
-                    $message : 'Expected string to be a primitive type, class or interface. Got %s.',
+                    $message : 'Expected string to be a primitive, class, interface, union or intersection. Got %s.',
                 (new TypeToString())($value),
             );
 
@@ -37,8 +36,11 @@ final class IsType implements Assertion
     public function __invoke(mixed $value): bool
     {
         return (new Either(
-            new IsPrimitive(),
-            new IsClassOrInterface(),
+            new IsBasicType(),
+            new Either(
+                new IsUnion(),
+                new IsIntersection(),
+            ),
         ))($value);
     }
 }
