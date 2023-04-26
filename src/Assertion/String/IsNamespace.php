55813be41838c5cedb1b189403fd3133636c8499
Luca Cantoreggi <luca@cn7r66.com>
2023-04-26 12:19:24 +0200

    Add IsNamespace assertion

---
+++ src/Assertion/String/IsNamespace.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vivarium\Assertion\String;
+
+use Vivarium\Assertion\Assertion;
+use Vivarium\Assertion\Exception\AssertionFailed;
+use Vivarium\Assertion\Helpers\TypeToString;
+use Vivarium\Assertion\Type\IsString;
+
+use function explode;
+use function preg_match;
+use function sprintf;
+
+/** @template-implements Assertion<string> */
+final class IsNamespace implements Assertion
+{
+    public function assert(mixed $value, string $message = ''): void
+    {
+        if (! $this($value)) {
+            $message = sprintf(
+                ! (new IsEmpty())($message) ?
+                    $message : 'Expected string to be namespace. Got %s.',
+                (new TypeToString())($value),
+            );
+
+            throw new AssertionFailed($message);
+        }
+    }
+
+    public function __invoke(mixed $value): bool
+    {
+        (new IsString())
+           ->assert($value);
+
+        $parts = explode('\\', $value);
+
+        foreach ($parts as $part) {
+            if (preg_match('/^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*$/', $part) !== 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
