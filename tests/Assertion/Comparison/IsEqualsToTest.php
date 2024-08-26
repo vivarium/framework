f57597a408c3aa493ccc1574e0d40d45e594026d
Luca Cantoreggi <luca@cn7r66.com>
2024-08-26 11:45:26 +0200

    Add tests provider

--- tests/Assertion/Comparison/IsEqualsToTest.php
+++ tests/Assertion/Comparison/IsEqualsToTest.php
@@ -11,8 +11,10 @@ declare(strict_types=1);
 namespace Vivarium\Test\Assertion\Comparison;
 
 use PHPUnit\Framework\TestCase;
+use stdClass;
 use Vivarium\Assertion\Comparison\IsEqualsTo;
 use Vivarium\Assertion\Exception\AssertionFailed;
+use Vivarium\Test\Equality\Stub\EqualityStub;
 
 /** @coversDefaultClass \Vivarium\Assertion\Comparison\IsEqualsTo */
 final class IsEqualsToTest extends TestCase
@@ -21,26 +23,51 @@ final class IsEqualsToTest extends TestCase
      * @covers ::__construct()
      * @covers ::assert()
      * @covers ::__invoke()
+     * 
+     * @dataProvider provideSuccess()
      */
-    public function testAssert(): void
+    public function testAssert(mixed $first, mixed $second): void
     {
         static::expectNotToPerformAssertions();
 
-        (new IsEqualsTo(5))
-            ->assert(5);
+        (new IsEqualsTo($first))
+            ->assert($second);
     }
 
     /**
      * @covers ::__construct()
      * @covers ::assert()
      * @covers ::__invoke()
+     * 
+     * @dataProvider provideFailure()
      */
-    public function testAssertException(): void
+    public function testAssertException(mixed $first, mixed $second, string $message): void
     {
         static::expectException(AssertionFailed::class);
-        static::expectExceptionMessage('Expected value to be equals to "RandomString". Got "Hello World"');
+        static::expectExceptionMessage($message);
 
-        (new IsEqualsTo('RandomString'))
-            ->assert('Hello World');
+        (new IsEqualsTo($first))
+            ->assert($second);
+    }
+
+    public static function provideSuccess(): array
+    {
+        $stdClass = new stdClass();
+
+        return [
+            ['Foo', 'Foo'],
+            [5, 5],
+            [-1, -1],
+            [$stdClass, $stdClass],
+            [new EqualityStub(), new EqualityStub()],
+        ];
+    }
+
+    public static function provideFailure(): array
+    {
+        return [
+            ['RandomString', 'Hello World', 'Expected value to be equals to "RandomString". Got "Hello World".'],
+            [new EqualityStub(), 5, 'Expected value to be equals to "Vivarium\Test\Equality\Stub\EqualityStub". Got 5.']
+        ];
     }
 }
