7a3af810198f4343bb7ceb852535646827d251ca
Luca Cantoreggi <luca@cn7r66.com>
2024-08-26 11:45:06 +0200

    Implement generic call to real assertion

--- src/Check/CheckIfString.php
+++ src/Check/CheckIfString.php
@@ -10,60 +10,29 @@ declare(strict_types=1);
 
 namespace Vivarium\Check;
 
-use Vivarium\Assertion\String\Contains;
-use Vivarium\Assertion\String\EndsWith;
-use Vivarium\Assertion\String\IsEmpty;
-use Vivarium\Assertion\String\IsLong;
-use Vivarium\Assertion\String\IsLongAtLeast;
-use Vivarium\Assertion\String\IsLongAtMax;
-use Vivarium\Assertion\String\IsLongBetween;
-use Vivarium\Assertion\String\IsNotEmpty;
-use Vivarium\Assertion\String\StartsWith;
+use Vivarium\Check\Check;
 
+/**
+ * @method static bool contains(string $string, string $substring)
+ * @method static bool endsWith(string $string, string $end)
+ * @method static bool isEmpty(string $string)
+ * @method static bool isLong(string $string, int $length, string $encoding = 'UTF-8')
+ * @method static bool isLongAtLeast(string $string, int $length, string $encoding = 'UTF-8')
+ * @method static bool isLongAtMax(string $string, int $length, string $encoding = 'UTF-8')
+ * @method static bool isLongBetween(string $string, int $min, int $max, string $encoding = 'UTF-8')
+ * @method static bool isNotEmpty(string $string)
+ * @method static bool startsWith(string $string, string $start)
+ */
 final class CheckIfString
 {
-    public static function contains(string $string, string $substring): bool
-    {
-        return (new Contains($substring))($string);
-    }
+    private static Check|null $check = null;
 
-    public static function endsWith(string $string, string $end): bool
+    public static function __callStatic($name, $arguments)
     {
-        return (new EndsWith($end))($string);
-    }
+        if (static::$check === null) {
+            static::$check = Check::string();
+        }
 
-    public static function isEmpty(string $string): bool
-    {
-        return (new IsEmpty())($string);
-    }
-
-    public static function isLong(string $string, int $length, string $encoding = 'UTF-8'): bool
-    {
-        return (new IsLong($length, $encoding))($string);
-    }
-
-    public static function isLongAtLeast(string $string, int $length, string $encoding = 'UTF-8'): bool
-    {
-        return (new IsLongAtLeast($length, $encoding))($string);
-    }
-
-    public static function isLongAtMax(string $string, int $length, string $encoding = 'UTF-8'): bool
-    {
-        return (new IsLongAtMax($length, $encoding))($string);
-    }
-
-    public static function isLongBetween(string $string, int $min, int $max, string $encoding = 'UTF-8'): bool
-    {
-        return (new IsLongBetween($min, $max, $encoding))($string);
-    }
-
-    public static function isNotEmpty(string $string): bool
-    {
-        return (new IsNotEmpty())($string);
-    }
-
-    public static function startsWith(string $string, string $start): bool
-    {
-        return (new StartsWith($start))($string);
+        return static::$check->$name(...$arguments);
     }
 }
