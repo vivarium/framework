dc8d3e53f7083165c0778067c368e71e58bb5279
Luca Cantoreggi <luca@cn7r66.com>
2024-08-28 01:10:01 +0200

    Add tests provider

--- tests/Assertion/Type/IsInterfaceTest.php
+++ tests/Assertion/Type/IsInterfaceTest.php
@@ -11,44 +11,94 @@ declare(strict_types=1);
 namespace Vivarium\Test\Assertion\Type;
 
 use PHPUnit\Framework\TestCase;
+use Traversable;
 use Vivarium\Assertion\Exception\AssertionFailed;
 use Vivarium\Assertion\Type\IsInterface;
+use Vivarium\Test\Assertion\Stub\Stub;
+use Vivarium\Test\Assertion\Stub\StubClass;
 
 /** @coversDefaultClass \Vivarium\Assertion\Type\IsInterface */
 final class IsInterfaceTest extends TestCase
 {
     /**
      * @covers ::assert()
-     * @covers ::__invoke()
+     * 
+     * @dataProvider provideSuccess()
      */
-    public function testAssert(): void
+    public function testAssert(string $type): void
     {
         static::expectNotToPerformAssertions();
 
         (new IsInterface())
-            ->assert('Traversable');
+            ->assert($type);
+    }
+
+    /** 
+     * @covers ::assert() 
+     *
+     * @dataProvider provideFailure()
+     * @dataProvider provideInvalid()
+     */
+    public function testAssertException(string|int $type, string $message): void
+    {
+        static::expectException(AssertionFailed::class);
+        static::expectExceptionMessage($message);
+
+        (new IsInterface())
+            ->assert($type);
     }
 
     /**
-     * @covers ::assert()
      * @covers ::__invoke()
+     * 
+     * @dataProvider provideSuccess()
      */
-    public function testAssertException(): void
+    public function testInvoke(string $type): void
     {
-        static::expectException(AssertionFailed::class);
-        static::expectExceptionMessage('Expected string to be interface name. Got "Foo".');
-
-        (new IsInterface())
-            ->assert('Foo');
+        static::assertTrue(
+            (new IsInterface())($type),
+        );
     }
 
-    /** @covers ::assert() */
-    public function testAssertWithoutString(): void
+    /**
+     * @covers ::__invoke()
+     * 
+     * @dataProvider provideFailure()
+     */
+    public function testInvokeFailure(string $type): void
     {
-        static::expectException(AssertionFailed::class);
-        static::expectExceptionMessage('Expected value to be string. Got integer.');
+        static::assertFalse(
+            (new IsInterface())($type)
+        );
+    }
 
-        (new IsInterface())
-            ->assert(42);
+    /** @return array<array<string>> */
+    public static function provideSuccess(): array
+    {
+        return [
+            [Stub::class],
+            [Traversable::class],
+        ];
+    }
+
+    public static function provideFailure(): array
+    {
+        return [
+            [
+                'NonExistentClass', 
+                'Expected string to be interface name. Got "NonExistentClass".'
+            ],
+            [
+                StubClass::class,
+                'Expected string to be interface name. Got "Vivarium\Test\Assertion\Stub".'
+            ]
+        ];
+    }
+
+    public static function provideInvalid(): array
+    {
+        return [
+            [42, 'Expected value to be string. Got integer.']
+        ];
     }
 }
