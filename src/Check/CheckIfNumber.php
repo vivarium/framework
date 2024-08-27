7ccd95f449e8ae57579d35b3c32c14f6f85c21fb
Luca Cantoreggi <luca@cn7r66.com>
2024-08-27 12:02:33 +0200

    Add numeric tests

--- src/Check/CheckIfNumber.php
+++ src/Check/CheckIfNumber.php
@@ -10,60 +10,28 @@ declare(strict_types=1);
 
 namespace Vivarium\Check;
 
-use Vivarium\Assertion\Numeric\IsGreaterOrEqualThan;
-use Vivarium\Assertion\Numeric\IsGreaterThan;
-use Vivarium\Assertion\Numeric\IsInClosedRange;
-use Vivarium\Assertion\Numeric\IsInHalfOpenRightRange;
-use Vivarium\Assertion\Numeric\IsInOpenRange;
-use Vivarium\Assertion\Numeric\IsLessOrEqualThan;
-use Vivarium\Assertion\Numeric\IsLessThan;
-use Vivarium\Assertion\Numeric\IsOutOfClosedRange;
-use Vivarium\Assertion\Numeric\IsOutOfOpenRange;
-
+/**
+ * @method static bool isGreaterOrEqualThan(int|float $number , int|float $compare)
+ * @method static bool isGreaterThan(int|float $number , int|float $compare)
+ * @method static bool isInClosedRange(int|float $number , int|float $min, int|float $max)
+ * @method static bool isInHalfOpenLeftRange(int|float $number , int|float $min, int|float $max)
+ * @method static bool isInHalfOpenRightRange(int|float $number , int|float $min, int|float $max)
+ * @method static bool isInOpenRange(int|float $number , int|float $min, int|float $max)
+ * @method static bool isLessOrEqualThan(int|float $number , int|float $compare)
+ * @method static bool isLessThan(int|float $number , int|float $compare)
+ * @method static bool isOutOfClosedRange(int|float $number , int|float $min, int|float $max)
+ * @method static bool isOutOfOpenRange(int|float $number , int|float $min, int|float $max)
+ */
 final class CheckIfNumber
 {
-    public static function isGreaterOrEqualThan(int|float $number, int|float $target): bool
-    {
-        return (new IsGreaterOrEqualThan($target))($number);
-    }
+    private static Check|null $check = null;
 
-    public static function isGreaterThan(int|float $number, int|float $target): bool
+    public static function __callStatic($name, $arguments)
     {
-        return (new IsGreaterThan($target))($number);
-    }
+        if (static::$check === null) {
+            static::$check = Check::numeric();
+        }
 
-    public static function isInClosedRange(int|float $number, int|float $min, int|float $max): bool
-    {
-        return (new IsInClosedRange($min, $max))($number);
-    }
-
-    public static function isInHalfOpenRightRange(int|float $number, int|float $min, int|float $max): bool
-    {
-        return (new IsInHalfOpenRightRange($min, $max))($number);
-    }
-
-    public static function isInOpenRange(int|float $number, int|float $min, int|float $max): bool
-    {
-        return (new IsInOpenRange($min, $max))($number);
-    }
-
-    public static function isLessOrEqualThan(int|float $number, int|float $target): bool
-    {
-        return (new IsLessOrEqualThan($target))($number);
-    }
-
-    public static function isLessThan(int|float $number, int|float $target): bool
-    {
-        return (new IsLessThan($target))($number);
-    }
-
-    public static function isOutOfClosedRange(int|float $number, int|float $min, int|float $max): bool
-    {
-        return (new IsOutOfClosedRange($min, $max))($number);
-    }
-
-    public static function isOutOfOpenRange(int|float $number, int|float $min, int|float $max): bool
-    {
-        return (new IsOutOfOpenRange($min, $max))($number);
+        return static::$check->$name(...$arguments);
     }
 }
--- tests/Check/CheckIfNumberTest.php
+++ tests/Check/CheckIfNumberTest.php
@@ -10,86 +10,40 @@ declare(strict_types=1);
 
 namespace Vivarium\Test\Check;
 
-use PHPUnit\Framework\TestCase;
 use Vivarium\Check\CheckIfNumber;
 
 /** @coversDefaultClass \Vivarium\Check\CheckIfNumber */
-final class CheckIfNumberTest extends TestCase
+final class CheckIfNumberTest extends CheckTestCase
 {
-    /** @covers ::isGreaterOrEqualThan() */
-    public function testIsGreaterOrEqualThan(): void
+    const NAMESPACE = 'Vivarium\Test\Assertion\Numeric';
+
+    /**
+     * @covers ::__callStatic()
+     * 
+     * @dataProvider provideMethods()
+     */
+    public function testCallStatic(string $method): void
     {
-        static::assertTrue(CheckIfNumber::isGreaterOrEqualThan(5, 3));
-        static::assertTrue(CheckIfNumber::isGreaterOrEqualThan(3, 3));
-        static::assertFalse(CheckIfNumber::isGreaterOrEqualThan(3, 5));
+        $this->doTest(
+            CheckIfNumber::class,
+            $method,
+            static::NAMESPACE
+        );
     }
 
-    /** @covers ::isGreaterThan() */
-    public function testIsGreaterThan(): void
+    public static function provideMethods(): array
     {
-        static::assertTrue(CheckIfNumber::isGreaterThan(5, 3));
-        static::assertFalse(CheckIfNumber::isGreaterThan(3, 3));
-        static::assertFalse(CheckIfNumber::isGreaterThan(3, 5));
-    }
-
-    /** @covers ::isInClosedRange() */
-    public function testIsInClosedRange(): void
-    {
-        static::assertTrue(CheckIfNumber::isInClosedRange(5, 0, 9));
-        static::assertTrue(CheckIfNumber::isInClosedRange(0, 0, 9));
-        static::assertTrue(CheckIfNumber::isInClosedRange(9, 0, 9));
-        static::assertFalse(CheckIfNumber::isInClosedRange(10, 0, 9));
-    }
-
-    /** @covers ::isInHalfOpenRightRange() */
-    public function testIsInHalfOpenRightRange(): void
-    {
-        static::assertTrue(CheckIfNumber::isInHalfOpenRightRange(5, 0, 9));
-        static::assertTrue(CheckIfNumber::isInHalfOpenRightRange(0, 0, 9));
-        static::assertFalse(CheckIfNumber::isInHalfOpenRightRange(9, 0, 9));
-        static::assertFalse(CheckIfNumber::isInHalfOpenRightRange(10, 0, 9));
-    }
-
-    /** @covers ::isInOpenRange() */
-    public function testIsInOpenRange(): void
-    {
-        static::assertTrue(CheckIfNumber::isInOpenRange(5, 0, 9));
-        static::assertFalse(CheckIfNumber::isInOpenRange(0, 0, 9));
-        static::assertFalse(CheckIfNumber::isInOpenRange(9, 0, 9));
-        static::assertFalse(CheckIfNumber::isInOpenRange(10, 0, 9));
-    }
-
-    /** @covers ::isLessOrEqualThan() */
-    public function testIsLessOrEqualThan(): void
-    {
-        static::assertTrue(CheckIfNumber::isLessOrEqualThan(3, 5));
-        static::assertTrue(CheckIfNumber::isLessOrEqualThan(5, 5));
-        static::assertFalse(CheckIfNumber::isLessOrEqualThan(6, 5));
-    }
-
-    /** @covers ::isLessThan() */
-    public function testIsLessThan(): void
-    {
-        static::assertTrue(CheckIfNumber::isLessThan(3, 5));
-        static::assertFalse(CheckIfNumber::isLessThan(5, 5));
-        static::assertFalse(CheckIfNumber::isLessThan(6, 5));
-    }
-
-    /** @covers ::isOutOfClosedRange() */
-    public function testIsOutOfClosedRange(): void
-    {
-        static::assertTrue(CheckIfNumber::isOutOfClosedRange(10, 0, 9));
-        static::assertFalse(CheckIfNumber::isOutOfClosedRange(5, 0, 9));
-        static::assertFalse(CheckIfNumber::isOutOfClosedRange(0, 0, 9));
-        static::assertFalse(CheckIfNumber::isOutOfClosedRange(9, 0, 9));
-    }
-
-    /** @covers ::isOutOfOpenRange() */
-    public function testIsOutOfOpenRange(): void
-    {
-        static::assertTrue(CheckIfNumber::isOutOfOpenRange(10, 0, 9));
-        static::assertTrue(CheckIfNumber::isOutOfOpenRange(0, 0, 9));
-        static::assertTrue(CheckIfNumber::isOutOfOpenRange(9, 0, 9));
-        static::assertFalse(CheckIfNumber::isOutOfOpenRange(5, 0, 9));
+        return [
+            ['isGreaterOrEqualThan'],
+            ['isGreaterThan'],
+            ['isInClosedRange'],
+            ['isInHalfOpenLeftRange'],
+            ['isInHalfOpenRightRange'],
+            ['isInOpenRange'],
+            ['isLessOrEqualThan'],
+            ['isLessThan'],
+            ['isOutOfClosedRange'],
+            ['isOutOfOpenRange'],
+        ];
     }
 }
