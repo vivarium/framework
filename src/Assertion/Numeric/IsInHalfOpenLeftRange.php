c633bbda0ca29537dc816f1649fb1778b8f2ee53
Luca Cantoreggi <luca@cn7r66.com>
2024-08-27 11:45:53 +0200

    Add IsInHalOpenLeftRange assertion

---
+++ src/Assertion/Numeric/IsInHalfOpenLeftRange.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of Vivarium
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2021 Luca Cantoreggi
+ */
+
+declare(strict_types=1);
+
+namespace Vivarium\Assertion\Numeric;
+
+use Vivarium\Assertion\Assertion;
+use Vivarium\Assertion\Exception\AssertionFailed;
+use Vivarium\Assertion\Helpers\TypeToString;
+use Vivarium\Assertion\String\IsEmpty;
+use Vivarium\Assertion\Var\IsNumeric;
+
+use function sprintf;
+
+/**
+ * @template T as int|float
+ * @template-implements Assertion<T>
+ */
+final class IsInHalfOpenLeftRange implements Assertion
+{
+    /**
+     * @param T $min
+     * @param T $max
+     */
+    public function __construct(private $min, private $max)
+    {
+        (new IsLessOrEqualThan($max))
+            ->assert($min, 'Lower bound must be lower than upper bound. Got (%1$s, %2$s].');
+    }
+
+    /** @psalm-assert T $value */
+    public function assert(mixed $value, string $message = ''): void
+    {
+        if (! $this($value)) {
+            $message = sprintf(
+                ! (new IsEmpty())($message) ?
+                    $message : 'Expected number to be in half open left range (%2$s, %3$s]. Got %s.',
+                (new TypeToString())($value),
+                (new TypeToString())($this->min),
+                (new TypeToString())($this->max),
+            );
+
+            throw new AssertionFailed($message);
+        }
+    }
+
+    /** @psalm-assert T $value */
+    public function __invoke(mixed $value): bool
+    {
+        (new IsNumeric())->assert($value);
+
+        return ($this->min < $value) && ($value <= $this->max);
+    }
+}
