1767476ab5faec62821e6ebe51cddc9cf6059bab
Luca Cantoreggi <luca@cn7r66.com>
2024-08-28 01:09:40 +0200

    Generic call to real assertion

--- tests/Check/CheckIfTypeTest.php
+++ tests/Check/CheckIfTypeTest.php
@@ -10,100 +10,45 @@ declare(strict_types=1);
 
 namespace Vivarium\Test\Check;
 
-use PHPUnit\Framework\TestCase;
-use stdClass;
-use Traversable;
 use Vivarium\Check\CheckIfType;
-use Vivarium\Test\Assertion\Stub\InvokableStub;
-use Vivarium\Test\Assertion\Stub\Stub;
-use Vivarium\Test\Assertion\Stub\StubClass;
-use Vivarium\Test\Assertion\Stub\StubClassExtension;
-
 /** @coversDefaultClass \Vivarium\Check\CheckIfType */
-final class CheckIfTypeTest extends TestCase
+final class CheckIfTypeTest extends CheckTestCase
 {
-    /** @covers ::implementsInterface() */
-    public function testImplementsInterface(): void
-    {
-        $mock = $this->createMock(Traversable::class);
+    const NAMESPACE = 'Vivarium\Test\Assertion\Type';
 
-        static::assertTrue(CheckIfType::implementsInterface($mock::class, Traversable::class));
-        static::assertFalse(CheckIfType::implementsInterface(stdClass::class, Traversable::class));
+    /**
+     * @covers ::__callStatic()
+     * 
+     * @dataProvider provideMethods()
+     */
+    public function testCallStatic(string $method): void
+    {
+        $this->doTest(
+            CheckIfType::class,
+            $method,
+            static::NAMESPACE
+        );
     }
 
-    /** @covers ::isAssignableTo() */
-    public function testIsAssignableTo(): void
+    public static function provideMethods(): array
     {
-        $types = [
-            [Stub::class, Stub::class],
-            [Stub::class, StubClass::class],
-            [Stub::class, StubClassExtension::class],
-            [StubClass::class, StubClassExtension::class],
-            ['float', 'int'],
-            ['string', 'string'],
-            ['callable', InvokableStub::class],
-            ['string', StubClass::class],
-            ['stdClass|' . StubClass::class, 'stdClass'],
-            ['stdClass|' . StubClass::class, StubClassExtension::class],
-            [Stub::class . '&' . InvokableStub::class, StubClassExtension::class],
+        return [
+            ['ImplementsInterface'],
+            ['IsAssignableTo'],
+            ['IsAssignableToClass'],
+            ['IsAssignableToIntersection'],
+            ['IsAssignableToPrimitive'],
+            ['IsAssignableToUnion'],
+            ['IsSubclassOf'],
+            ['IsBasicType'],
+            ['IsClass'],
+            ['IsClassOrInterface'],
+            ['IsInterface'],
+            ['IsIntersection'],
+            ['IsNamespace'],
+            ['IsPrimitive'],
+            ['IsType'],
+            ['IsUnion'],
         ];
-
-        foreach ($types as $type) {
-            static::assertTrue(CheckIfType::isAssignableTo($type[1], $type[0]));
-        }
-
-        static::assertFalse(CheckIfType::isAssignableTo(Stub::class, StubClassExtension::class));
-    }
-
-    /** @covers ::isAssignableToClass() */
-    public function testIsAssignableToClass(): void
-    {
-        static::assertTrue(CheckIfType::isAssignableToClass(Stub::class, Stub::class));
-        static::assertTrue(CheckIfType::isAssignableToClass(StubClass::class, Stub::class));
-        static::assertFalse(CheckIfType::isAssignableToClass(Stub::class, stdClass::class));
-    }
-
-    /** @covers ::isAssignableToIntersection() */
-    public function testIsAssignableToIntersection(): void
-    {
-        $intersection = Stub::class . '&' . InvokableStub::class;
-
-        static::assertTrue(CheckIfType::isAssignableToIntersection(StubClassExtension::class, $intersection));
-        static::assertFalse(CheckIfType::isAssignableToIntersection(Stub::class, $intersection));
-    }
-
-    /** @covers ::isAssignableToPrimitive() */
-    public function testIsAssignableToPrimitive(): void
-    {
-        $types = [
-            ['float', 'int'],
-            ['string', 'string'],
-            ['string', StubClass::class],
-            ['object', StubClass::class],
-            ['callable', StubClassExtension::class],
-        ];
-
-        foreach ($types as $type) {
-            static::assertTrue(CheckIfType::isAssignableToPrimitive($type[1], $type[0]));
-        }
-
-        static::assertFalse(CheckIfType::isAssignableTo(stdClass::class, 'int'));
-    }
-
-    /** @covers ::isAssignableToUnion() */
-    public function testIsAssignableToUnion(): void
-    {
-        $union = $union = 'stdClass|' . StubClass::class;
-
-        static::assertTrue(CheckIfType::isAssignableToUnion(stdClass::class, $union));
-        static::assertTrue(CheckIfType::isAssignableToUnion(StubClass::class, $union));
-        static::assertFalse(CheckIfType::isAssignableToUnion('int', $union));
-    }
-
-    /** @covers ::isSubclassOf() */
-    public function testIsSubclassOf(): void
-    {
-        static::assertTrue(CheckIfType::isSubclassOf(StubClass::class, Stub::class));
-        static::assertFalse(CheckIfType::isSubclassOf(Stub::class, stdClass::class));
     }
 }
